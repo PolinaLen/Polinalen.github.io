02f175f58feff30f7ac1741f2585f62a68fc5ff5
PolinaLen <Polinaleeeen@gmail.com>
2017-07-06 11:09:40 +0600

    end

--- EKZIS/js/custom.js
+++ EKZIS/js/custom.js
@@ -10,4 +10,29 @@ $(document).ready(function(){
 	    }, 1000); // можно менять время
 	    return false;
 	});
+
+	$('#p1c').click(function(){
+		$('#1c').modal();
+	});
+	$('#pwebs').click(function(){
+		$('#webs').modal();
+	});
+	$('#pprog').click(function(){
+		$('#prog').modal();
+	});
+	$('#psoftware').click(function(){
+		$('#software').modal();
+	});
+	$('#psu').click(function(){
+		$('#su').modal();
+	});
+	$('#ppoly').click(function(){
+		$('#poly').modal();
+	});
+	$('#phelp').click(function(){
+		$('#help').modal();
+	});
+	$('#psupport').click(function(){
+		$('#support').modal();
+	});
 })
